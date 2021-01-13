ea6d7a78387665e98d08dca9e47502b37e75466a
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-01-13 21:49:19 +0100

    Implement function to set the state to empty array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import NameList from './components/NameLists'
+import data from './components/data'
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: data
+        }
+    }
+
+    handleClear = () => {
+        this.setState({
+            name: []
+        })
+    }
     render() {
+        const dataComp = this.state.name.map(item => <NameList key={item.id} name={item.name} />)
       return (
-        <div className="container mt-4 temp">
+        <div className="container">
             <div className='row'>
                 <div className='col-6 offset-3'>
-                    <h2>Hello again.</h2>
+                    {dataComp}
                 </div>
             </div>
+            <button className="" onClick={this.handleClear}>clear item</button>
         </div>
       )
     }
--- src/components/NameLists.js
+++ src/components/NameLists.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class NameLists extends Component {
-    render() {
-        return (
-            <div>
-            </div>
-        )
-    }
+function NameLists(props) {
+    return (
+        <div className="container">
+          <h5 
+            className="item text-capitalize m-4">
+            {props.name}
+          </h5>  
+        </div>
+    )
 }
 
 
